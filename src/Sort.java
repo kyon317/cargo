83425a679e520780a5767b7a5beb9e27daf4f19d
CarpenterGlue <s179036326@gmail.com>
2024-02-05 16:58:00 -0800

    - Quick Sort

--- src/Sort.java
+++ src/Sort.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Sort {
     int[] temp;
@@ -86,5 +87,40 @@ public class Sort {
         }
     }
 
+    // quickSort
+    private void quickSort(int[] nums, int lo, int hi){
+        if(lo >= hi) return;
+        int p = partition(nums, lo, hi);
+        quickSort(nums, lo, p - 1);
+        quickSort(nums, p + 1, hi);
+    }
 
+    private int partition(int[] nums, int lo, int hi){
+        int pivot = nums[lo];
+        int i = lo + 1, j = hi;
+        while(i <= j){
+            while(i <= hi && nums[i] <= pivot){
+                i++;
+            }
+            while(j >= i && nums[j] > pivot) j--;
+            if(i >= j) break;
+            swap(nums, i, j);
+        }
+        swap(nums, lo, j);
+        return j;
+    }
+    private static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private static void shuffle(int[] nums) {
+        Random rand = new Random();
+        int n = nums.length;
+        for (int i = 0 ; i < n; i++) {
+            // 生成 [i, n - 1] 的随机数
+            int r = i + rand.nextInt(n - i);
+            swap(nums, i, r);
+        }
+    }
 }
